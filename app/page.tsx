e2adc992c4a58afd6affc8a7a9b6496cb62c8498
varia <ltnhan2610@gmail.com>
2025-11-14 15:09:02 +0700

    Style tagline

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Travel Smarter, Not Harder</h1>
-        <p>
+    <div className="flex flex-col items-center justify-center gap-6 py-20">
+      <div className="flex flex-col gap-2 text-center">
+        <h1 className="text-4xl leading-none font-semibold tracking-wider">
+          Travel Smarter, Not Harder
+        </h1>
+        <p className="text-muted-foreground text-lg leading-none font-normal tracking-normal">
           Make every trip effortless. Tripzy lets you book rides and plan
           journeys with ease
         </p>
