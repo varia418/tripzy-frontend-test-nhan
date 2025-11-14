e34c6721a9167425948547c0ef589f8a712a1479
varia <ltnhan2610@gmail.com>
2025-11-14 12:06:22 +0700

    Add header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const nunitoSans = Nunito_Sans({
   variable: "--font-nunito-sans",
@@ -21,8 +23,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${nunitoSans.className} antialiased`}>
         <div>
-          <div className="h-[495px] w-full bg-linear-to-b from-[#F5F8FF] to-[#DBF5FF]">
-            <div>Tripzy</div>
+          <div className="absolute top-0 left-0 -z-10 h-[495px] w-full bg-linear-to-b from-[#F5F8FF] to-[#DBF5FF]"></div>
+          <div className="container mx-auto pt-8">
+            <Link href="/" className="inline-block">
+              <Image src="/logo.png" width={126} height={40} alt="logo" />
+            </Link>
           </div>
           {children}
         </div>
